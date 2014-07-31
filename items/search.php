eb1fa488131277861428e3c9123f411048dbd3d6
Rachel Donahue <donahrm@gmail.com>
2014-07-31 16:27:59 -0400

    added adv-search wrapper :gift:

--- items/search.php
+++ items/search.php
@@ -10,10 +10,12 @@ echo head(array('title' => $pageTitle,
     <?php echo public_nav_items(); ?>
 </nav>
 
-<p class="disclaimer">Please note that full-text search is only available for items which have successfully had their text extracted. Due to the nature of the materials, many documents are not OCR-compatible.</p>
+<div id="item-search-wrapper">
+<p class="disclaimer">Please note that full-text search is only available for items which have successfully had their text extracted. <br />Due to the nature of the materials, many documents are not OCR-compatible.</p>
 
 <?php echo $this->partial('items/search-form.php',
     array('formAttributes' =>
         array('id'=>'advanced-search-form'))); ?>
+        </div>
 
 <?php echo foot(); ?>
