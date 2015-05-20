f75c534932af0c13f32f6fb90eed57f13055bab9
Rachel Donahue <donahrm@gmail.com>
2015-05-20 14:36:54 -0400

    changes to make sidebar items an exhibit layout

--- custom.php
+++ custom.php
@@ -485,4 +485,59 @@ function searchPages($arr,$field,$value)
        }
        return $vals;
     }
+
+function nal_exhibit_builder_render_exhibit_page ($exhibitPage = null)
+{
+    if ($exhibitPage === null) {
+        $exhibitPage = get_current_record('exhibit_page');
+    }
+    
+    $blocks = $exhibitPage->ExhibitPageBlocks;
+    $rawAttachments = $exhibitPage->getAllAttachments();
+    $attachments = array();
+    foreach ($rawAttachments as $attachment) {
+        $attachments[$attachment->block_id][] = $attachment;
+    }
+    foreach ($blocks as $index => $block) {
+        $layout = $block->getLayout();
+        if ($layout->id != "nal-sidebar") {
+        echo '<div class="exhibit-block layout-' . html_escape($layout->id) . '">';
+        echo get_view()->partial($layout->getViewPartial(), array(
+            'index' => $index,
+            'options' => $block->getOptions(),
+            'text' => $block->text,
+            'attachments' => array_key_exists($block->id, $attachments) ? $attachments[$block->id] : array()
+        ));
+        echo '</div>'; }
+    }
+}
+
+function nal_exhibit_builder_render_exhibit_sidebar ($exhibitPage = null)
+{
+    if ($exhibitPage === null) {
+        $exhibitPage = get_current_record('exhibit_page');
+    }
+    
+    $blocks = $exhibitPage->ExhibitPageBlocks;
+    $rawAttachments = $exhibitPage->getAllAttachments();
+    $attachments = array();
+    foreach ($rawAttachments as $attachment) {
+        $attachments[$attachment->block_id][] = $attachment;
+    }
+    foreach ($blocks as $index => $block) {
+        $layout = $block->getLayout();
+        if ($layout->id == "nal-sidebar"){
+                
+                echo get_view()->partial($layout->getViewPartial(), array(
+                    'index' => $index,
+                    'options' => $block->getOptions(),
+                    'text' => $block->text,
+                    'attachments' => array_key_exists($block->id, $attachments) ? $attachments[$block->id] : array()
+                ));
+        }
+    }
+}
 ?>
+
+?>
+
