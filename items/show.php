da124e242f25d217551c51e5a5844f37dfc1269a
Rachel Donahue <donahrm@gmail.com>
2015-08-31 11:58:13 -0400

    cleaned up PDF switch

--- items/show.php
+++ items/show.php
@@ -32,12 +32,11 @@
 
 				echo $html;
 				unset($html);
+				
 			} else {
-				echo "<h2>" . metadata($files[0], 'size') . "</h2>";
 				echo file_markup($files[0], array('imageSize' => 'fullsize', 'imgAttributes'=>array('alt'=>'Image for the first content page of the item, linking to the full file.', 'title'=>metadata('item', array('Dublin Core', 'Title')))));
-
-
 			} 
+
 			unset($files[0]);
 			?>
 
@@ -53,71 +52,17 @@
 			<?php if (metadata('item','item_type_id') == 6) {
 				echo file_markup($files, array('imageSize' => 'fullsize', 'imgAttributes'=>array('alt'=>'Image (illustration or photograph) for this item, linking to higher res image.', 'title'=>metadata('item', array('Dublin Core', 'Title')))));
 			} else {
-					
-					// $html = '<div class="item-file">';
-					// $html += "<h2>" . metadata($files[0], 'size') . "LARGER" . metadata($files[0], 'MIME Type') ."</h2>";
-					// "<a href=\"" . $files[0]->getWebPath('original') .  'title="' . metadata('item', array('Dublin Core', 'Title')) . '" alt="View the PDF.">';
-     //                $image = file_image('fullsize', array(alt => '"Thumbnail for the first (or only) page of "' . metadata('item', array('Dublin Core', 'Title')) . '"'), $files[0]);
-     //                $html += $image;
-     //                $html += "</a></div>";  			
 
-					$sizeTest = null;
-					if(metadata($files[0], 'size') < 10000000) { 
-						$sizeTest = "true"; 
-					} else { 
-						$sizeTest = "false";
-					}
+				if (metadata($files[0], 'size') > 10000000 && metadata($files[0], 'MIME Type') == "application/pdf") {
+					echo "<div class=\"item-file\">";
+					echo "<p><strong>This PDF is too  large to display. Click on the image below to view the PDF.</strong></p>";
+					echo "<a href=\"" . $files[0]->getWebPath('original') .  '" title="' . metadata('item', array('Dublin Core', 'Title')) . '" alt="View the PDF.">'. file_image('fullsize', array(alt => 'Thumbnail for the first (or only) page of ' . metadata('item', array('Dublin Core', 'Title'))), $files[0]) . "</a></div>";	
 
-					$pdfTest = null;
-					if(metadata($files[0], 'MIME Type') == "application/pdf") { 
-						$pdfTest = "true"; 
-					} else { 
-						$pdfTest = "false";
-					} 
-					// if ($sizeTest == "false" && $pdfTest == "true")
-
-					if (metadata($files[0], 'size') > 10000000 && metadata($files[0], 'MIME Type') == "application/pdf") {
-					echo "<div>"	;
-					echo "<p><span><strong>MIMEtype: </strong>" . metadata($files[0], 'MIME Type') . "</span></p>";
-					echo "<p><span><strong>Size: </strong>" . metadata($files[0], 'size') . " (Type: " . gettype(metadata($files[0], 'size')) . ")</span></p>";
-					echo "<p><span><strong>Is less than 10000000? </strong>" . $sizeTest . "</span></p>";
-					echo "<p><span><strong>Is a PDF? </strong>" . $pdfTest . "</span></p>";
-					echo "<p>This is supposed to be for if the filesize is than 10000000.</p>";
-					echo "<p><strong>Path: </strong>". $files[0]->getWebPath('original'). "</div>";
-					echo "<div class=\"item-file\">" . "<a href=\"" . $files[0]->getWebPath('original') .  '" title="' . metadata('item', array('Dublin Core', 'Title')) . '" alt="View the PDF.">'. file_image('fullsize', array(alt => 'Thumbnail for the first (or only) page of ' . metadata('item', array('Dublin Core', 'Title'))), $files[0]) . "</a></div>";
-					
-
-					
 				} else {
-
-					// $sizeTest = null;
-					// if(metadata($files[0], 'size') < 100000) { 
-					// 	$sizeTest2 = "true"; 
-					// } else { 
-					// 	$sizeTest2 = "false";
-					// } 
-
-					// $pdfTest2 = null;
-					// if(metadata($files[0], 'MIME Type') == "application/pdf") { 
-					// 	$pdfTest2 = "true"; 
-					// } else { 
-					// 	$pdfTest2 = "false";
-					// } 
-
-					echo "<div>"	;
-					echo  "<p><span><strong>MIMEtype: </strong>" . metadata($files[0], 'MIME Type') . "</span></p>";
-					echo  "<p><span><strong>Size: </strong>" . metadata($files[0], 'size') . " (Type: " . gettype(metadata($files[0], 'size')) . ")</span></p>";
-					echo  "<p><span><strong>Is less than 10000?</strong> " . $sizeTest . "</span></p>";
-					echo "<p>This is supposed to be for if the filesize is less than than 10000000.</p>";
-					echo "<p><span><strong>Is a PDF? </strong>" . $pdfTest . "</span></p>";
 					echo file_markup($files[0], array('imageSize' => 'fullsize', 'imgAttributes'=>array('alt'=>'Image for the first content page of the item, linking to the full file.', 'title'=>metadata('item', array('Dublin Core', 'Title')))));
-					
-					echo "</div>";
 
-}
-				// echo "<h2>" . metadata($files[0], 'size') . "SMALLER" . metadata($files[0], 'MIME Type') . " " . gettype(metadata($files[0], 'size'))."</h2>";
-				// 	
-			
+				}
+
 
 			} 
 			unset($files[0]); ?>
